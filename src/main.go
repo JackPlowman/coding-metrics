27a2097f853ffda654de5cae0dc8d88f3ba936e2
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-18 20:48:36 +0100

    update

--- src/main.go
+++ src/main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-	"github.com/ajstarks/svgo"
 	"os"
+
+	"github.com/ajstarks/svgo"
 )
 
+// main is the entry point for the application.
 func main() {
 	width := 500
 	height := 500
-	file, err := os.Create("output.svg")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+	file := createLocalFile()
 	canvas := svg.New(file)
 	canvas.Start(width, height)
 	canvas.Circle(width/2, height/2, 100)
 	canvas.Text(width/2, height/2, "Hello, SVG", "text-anchor:middle;font-size:30px;fill:white")
 	canvas.End()
+}
+
+// createLocalFile creates a new SVG file in the local directory.
+func createLocalFile() *os.File {
+	file, err := os.Create("output.svg")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	return file
 }
