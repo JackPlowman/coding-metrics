81562f3e6f1b66b1cd6ab4a2d83c5a8b8c60163e
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-18 20:42:37 +0100

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/JackPlowman/coding-metrics
 
 go 1.25.0
+
+require github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b
--- src/main.go
+++ src/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"github.com/ajstarks/svgo"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	width := 500
+	height := 500
+	file, err := os.Create("output.svg")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	canvas := svg.New(file)
+	canvas.Start(width, height)
+	canvas.Circle(width/2, height/2, 100)
+	canvas.Text(width/2, height/2, "Hello, SVG", "text-anchor:middle;font-size:30px;fill:white")
+	canvas.End()
 }
