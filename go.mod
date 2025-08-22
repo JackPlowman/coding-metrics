739297cd8fd079dd8a54444b7f80bba2063a9277
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-22 16:19:55 +0100

    update

--- go.mod
+++ go.mod
@@ -4,12 +4,7 @@ go 1.25.0
 
 require (
 	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b
-	github.com/shurcooL/githubv4 v0.0.0-20240727222349-48295856cce7
 	go.uber.org/zap v1.27.0
-	golang.org/x/oauth2 v0.30.0
 )
 
-require (
-	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
-)
+require go.uber.org/multierr v1.10.0 // indirect
--- src/github_queries.go
+++ src/github_queries.go
@@ -1,38 +1,85 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
-
-	"github.com/shurcooL/githubv4"
-	"golang.org/x/oauth2"
 )
 
 func getPullRequestTotal() {
-	src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("INPUT_GITHUB_TOKEN")},
-	)
-	httpClient := oauth2.NewClient(context.Background(), src)
-	client := githubv4.NewClient(httpClient)
-
-	var query struct {
-		User struct {
-			PullRequests struct {
-				TotalCount int
-			}
-		} `graphql:"user(login: $login)"`
+	username := "JackPlowman" // TODO: get username from GITHUB_TOKEN
+	url := fmt.Sprintf("https://api.github.com/search/issues?q=author:%s+type:pr", username)
+	
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("Failed to create request for pull request total: %v\n", err)
+		return
 	}
 
-	vars := map[string]interface{}{
-		"login": githubv4.String("JackPlowman"), // TODO: get username from GITHUB_TOKEN
+	// Add GitHub token if available for higher rate limits
+	if token := os.Getenv("INPUT_GITHUB_TOKEN"); token != "" {
+		req.Header.Set("Authorization", "Bearer "+token)
 	}
 
-	err := client.Query(context.Background(), &query, vars)
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Printf("Failed to query GitHub API for pull request total: %v\n", err)
 		return
 	}
+	defer resp.Body.Close()
 
-	fmt.Printf("Total pull requests by %s: %d\n", vars["login"], query.User.PullRequests.TotalCount)
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("GitHub API returned status %d for pull request total\n", resp.StatusCode)
+		return
+	}
+
+	var result struct {
+		TotalCount int `json:"total_count"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		fmt.Printf("Failed to decode pull request total response: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Total pull requests by %s: %d\n", username, result.TotalCount)
 }
+
+// getUserAvatarURL fetches the user's avatar URL from GitHub REST API
+func getUserAvatarURL(username string) (string, error) {
+	url := fmt.Sprintf("https://api.github.com/users/%s", username)
+	
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %w", err)
+	}
+
+	// Add GitHub token if available for higher rate limits
+	if token := os.Getenv("INPUT_GITHUB_TOKEN"); token != "" {
+		req.Header.Set("Authorization", "Bearer "+token)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to make request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("GitHub API returned status %d", resp.StatusCode)
+	}
+
+	var user struct {
+		AvatarURL string `json:"avatar_url"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&user); err != nil {
+		return "", fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return user.AvatarURL, nil
+}
+
