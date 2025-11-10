31457aed64efda964ce9d117ef5ea1c380a82ca0
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-11-10 19:25:16 +0000

    update

--- src/svg_content.go
+++ src/svg_content.go
@@ -47,10 +47,14 @@ func generateSVGContent() []svg.Element {
 // Generate profile section of svg
 func generateProfileSection(userInfo *GitHubUserInfo) svg.Element {
 	yearsAgo := time.Since(userInfo.JoinedGitHub).Hours() / 24 / 365
+	
+	// Fetch avatar as base64 data URI for proper embedding in SVG
+	avatarDataURI := fetchAvatarAsBase64(userInfo.AvatarURL)
+	
 	return svg.G().AppendChildren(
 		// Use <foreignObject> for rounded avatar if <image> can't have rounded corners
 		svg.Image().
-			Href(svg.String(userInfo.AvatarURL)).
+			Href(svg.String(avatarDataURI)).
 			Width(svg.Px(24)).Height(svg.Px(24)).
 			X(svg.Px(18)).Y(svg.Px(28)).
 			Class(svg.String("avatar")),
